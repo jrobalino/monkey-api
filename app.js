fdc9b488a5ff308ca109f35f4083409b41bdb2cd
jrobalino <javier.robalino@gmail.com>
2020-07-15 15:20:44 -0500

    Add body parsing.

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 app.use((req, res, next) => {
 	res.header('Access-Control-Allow-Origin', '*'); // this could be limited to app url

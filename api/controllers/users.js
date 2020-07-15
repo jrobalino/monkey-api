a547fa9c55b2844226c60f7b678ed03b75892d08
jrobalino <javier.robalino@gmail.com>
2020-07-15 17:58:09 -0500

    Add login.

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -1,6 +1,7 @@
 const User = require('../models/user');
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 exports.users_sign_up = (req, res, next) => {
 	User.find({ email: req.body.email })
@@ -38,6 +39,49 @@ exports.users_sign_up = (req, res, next) => {
 				});
 			}
 		});	
+};
+
+exports.users_log_in = (req, res, next) => {
+	User.findOne({ email: req.body.email })
+		.exec()
+		.then(user => {
+			if (user.length < 1) {
+				return res.status(401).json({
+					message: 'Authorization failed'
+				});
+			}
+			bcrypt.compare(req.body.password, user.password, (err, result) => {
+				if (err) {
+					return result.status(401).json({
+						message: 'Authorization failed'
+					})
+				} else if (result) {
+					const token = jwt.sign(
+						{
+							email: user.email,
+							userId: user._id
+						},
+						process.env.JWT_KEY,
+						{
+							expiresIn: '1h'
+						}
+					);
+					return res.status(200).json({
+						message: 'Authorization succeeded',
+						token: token
+					});
+				} else {
+					res.status(401).json({
+						message: 'Authorization failed'
+					});
+				}
+			});
+		})
+		.catch(err => {
+			res.status(401).json({
+				message: 'Authorization failed'
+			});
+		});
 };
 
 exports.users_delete = (req, res, next) => {
--- api/routes/users.js
+++ api/routes/users.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const UsersController = require('../controllers/users');
 
 router.post('/signup', UsersController.users_sign_up);
+router.post('/login', UsersController.users_log_in);
 router.delete('/:userId', UsersController.users_delete);
 
 module.exports = router;
